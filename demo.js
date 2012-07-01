54c57bf85e437d8aa7909f7de6a10ff4dc5d5c1b
Pablo Molina <pablo.molinacandel@gmail.com>
2012-07-01 14:31:35 +0200

    DB config

--- demo.js
+++ demo.js
@@ -1,12 +1,21 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+    var pg = require('pg');
+
+    pg.connect(process.env.DATABASE_URL, function(err, client) {
+      var query = client.query('SELECT * FROM players');
+    
+      query.on('row', function(row) {
+        response.send(JSON.stringify(row));
+      });
+    });
+
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
